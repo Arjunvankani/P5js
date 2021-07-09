337b6d0ba4f1783e465d6d62decd11c9348bac29
Vankani Arjun <vankaniarjun0103@gmail.com>
2021-07-09 14:31:50 +0530

    Second Commit

---
+++ Input/s57.js
@@ -0,0 +1,17 @@
+
+let scale;
+
+function setup() {
+  createCanvas(1500, 720);
+  noStroke();
+  scale = width/20;
+}
+
+function draw() { 
+  let i;
+  for ( i = 0; i < scale; i++) {
+    colorMode(RGB, (i+1) * scale * 10);
+    fill(millis()%((i+1) * scale * 10));
+    rect(i*scale, 0, scale, height);
+  }
+}
